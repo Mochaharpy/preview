f436dfee5a368e450464c1f1aea32b91ae7cc36b
Mochaharpy <colton.ong@gmail.com>
2026-03-20 22:27:20 -0700

    editted and added some stuff

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+let currentFile = 'index.html';
+require.config({ paths: { vs: 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.44.0/min/vs' } });
+require(['vs/editor/editor.main'], function () {
+    monaco.editor.defineTheme('one-dark', {
+        base: 'vs-dark', inherit: true,
+        rules: [{ token: 'keyword', foreground: 'c678dd' }],
+        colors: { 'editor.background': '#282c34', 'editor.lineHighlightBackground': '#2c313a' }
+    });
+    editor = monaco.editor.create(document.getElementById('monaco-anchor'), { theme: 'one-dark', automaticLayout: true });
+    editor.onDidChangeModelContent(() => { 
+      updateFile(currentFile, editor.getValue())
+     });
+});
+
 function updateFile(path, content, action = 'UPDATE_FILE') {
   navigator.serviceWorker.controller.postMessage({
     type: action,
@@ -6,6 +20,18 @@ function updateFile(path, content, action = 'UPDATE_FILE') {
       content: content
     }
   });
+}
+
+function reloadPreview() {
+  const preview = document.getElementById("previewer");
+  preview.src = preview.src
+}
+
+function previewFile(formData) {
+  const data = new FormData(formData);
+
+  const preview = document.getElementById("previewer");
+  preview.src = "./preview/preview-project/" + data.get("path")
 }
 
 if ('serviceWorker' in navigator) navigator.serviceWorker.register("../serviceworker.js", { scope: '/' })
